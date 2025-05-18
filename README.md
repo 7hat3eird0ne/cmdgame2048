d7fb2bf388c2e0bf93f01ce153084c86e419de62
7hat3eird0ne <7hat3eird0ne@gmail.com>
2025-05-18 12:46:48 +0100

    Undoing supported - docstrings added to all functions and methods - powerup_mode introduced,  where 1 allows power ups to be used and 2 allows unlimited amount of undos be done

--- README.md
+++ README.md
@@ -4,5 +4,10 @@ This is a recreation of the [2048 game](https://play2048.co/) by Gabriele Circul
 To play, press either WASD or arrow keys. This moves all the tiles in a certain direction. If two same tiles move and are next to each other in the moving direction, they merge to a higher power of two. If three tiles are next to each other, only the first two merge. If there are four same tiles next to each other, the pairs merge to the higher powers.
 
 You get score from merging two tiles together. The goal is to eventually make a 2048 tile and then reach the highest score you can. You lose moment you can't merge or move any tiles in any direction.
-### Powerups
-I plan to add a mode with power ups, activated by writing "p" right before starting the game.
+### power-ups
+There are three types of power-ups - undo a move, swap two tiles, and delete tiles by number. Only the first is supported right now.
+- Undo a move -  Activated by doing SHIFT+U. Undoes the last move you did, you can't undo two moves in a row. You start with 2 uses and need to make a 128 tile to get more.
+- (not supported) - Activated by doing SHIFT+S. Pushing moving keys moves coordinates on bottom. Pressing SHIFT selects the tile coordinates point to. Once two tiles are selected, the selected tiles are swapped. Press R to quit the swap selection mode. Trying to select an empty tile fails. You start with 1 use and need to make a 256 tile to get more.
+- (not supported) - Activated by doing SHIFT+D. Pushing moving keys moves coordinates on bottom. Pressing SHIFT selects the tile coordinates point to. If the selected tile is not empty, it will remove all tiles with that number. Press R to quit delete selection mode. You start with no uses and need to make a 512 tile to get more.
+
+The power-ups are implemented based of the remake of the 2048 game made by the same creator as of the original.
--- cmdgame2048.py
+++ cmdgame2048.py
@@ -3,30 +3,38 @@ from pynput import keyboard
 import os
 import typing
 
-clean = lambda : os.system("cls") if os.name == "nt" else os.system("clear")
-
-SpawnOutput = typing.Literal[-1, 1, 2]
-MoveOutput = typing.Literal[-1, 0, 1, 2]
-Grid = typing.List[typing.List[int]]
-GameState = typing.Literal[-3, -2, -1, 1, 2, 3]
-
 class Game2048:
+    """Class describing a game of 2048
+    
+    Methods:
+    self.public_move(direction: int) - Makes a move in the said direction
+    self.restart() Restarts the game
+    self.undo() - Undoes the last move if supported
+    self.swap() - Swaps two tiles if supported
+    self.delete() - Deletes all tiles with that number if supported
+    """
     def __str__(self)-> str:
-        additional_info = ""
+        """Return a string representation of the board and statistics"""
+        additional_info: str = ""
         if abs(self.game_state) == 2:
             additional_info = " (GAME WON)"
         elif abs(self.game_state) == 3:
             additional_info = " (CUSTOM GRID USED)"
-        result: str = f"Score: {self.score}, Moves: {self.moves}{additional_info}"
-        max_character_lengths = []
+        secondary_add_info = ""
+        if self.powerups:
+            secondary_add_info = f" Powerups used: {self.powerups_used}"
+        if self.practice:
+            secondary_add_info += " (PRACTICE MODE)"
+        result: str = f"Score: {self.score}, Moves: {self.moves}{secondary_add_info}{additional_info}"
+        max_character_lengths: list[int] = []
         for i in zip(*self.grid):
             max_character_lengths.append(len(self.tiles[max(i)]))
         for i in self.grid:
             result += "\n"
             first: bool = True
             for k in range(len(i)):
-                j = i[k]
-                column_max_length = max_character_lengths[k]
+                j: int = i[k]
+                column_max_length: int = max_character_lengths[k]
                 tile_string: str = self.tiles[j]
                 padded_length: int = column_max_length - len(tile_string)
                 front_padding: int = padded_length//2 + 1
@@ -39,12 +47,14 @@ class Game2048:
                 else:
                     tile_string = "|" + tile_string
                 result += tile_string
+        result += f"\nUndos left: {self.undos_left}, swaps left: {self.swaps_left}, deletes left: {self.deletes_left}"
         if self.game_state < 0:
             result += "\nGAME OVER"
 
         return result
 
-    def _spawn(self)-> SpawnOutput:
+    def _spawn(self)-> typing.Literal[-1, 1, 2]:
+        """Spawn a 2 or 4 tile at random empty position of the grid"""
         flattened_grid: typing.List[int] = []
         for i in self.grid:
             flattened_grid.extend(i)
@@ -71,7 +81,15 @@ class Game2048:
         self.grid = [flattened_grid[i:i+4] for i in range(0, 16, 4)]
         return chosen_level
     
-    def _move(self, direction: int, check_only: bool = False)-> MoveOutput:
+    def _move(self, direction: int, check_only: bool = False)-> typing.Literal[-1, 0, 1, 2]:
+        """Do a raw move in any direction
+        
+        Parameters:
+
+            direction = an integer, 0 means left, going up by 1 rotates the direction by 90 degrees clockwise
+
+            check_only = a boolean, denoting if the move should change the board and statistics, used by _check method
+        """
         #left - 0, up - 1, right - 2, down - 3
         direction %= 4
         rotated_grid = self.grid.copy()
@@ -82,7 +100,7 @@ class Game2048:
         definitely_changed: bool = False
         new_tiles: typing.List[str] = self.tiles.copy()
         new_score = self.score
-        new_rotated_grid: Grid = []
+        new_rotated_grid: typing.List[typing.List[int]] = []
         for row_index in range(4):
             row = rotated_grid[row_index].copy()
             if row.count(0) != 0:
@@ -109,6 +127,12 @@ class Game2048:
                     while tile + 1 >= len(new_tiles):
                         new_tiles.append(str(2*int(new_tiles[-1])))
                     new_score += int(new_tiles[tile + 1])
+                    if tile == 6 and not self.practice:
+                        self.undos_left = min(self.undos_left + 1, 2)
+                    elif tile == 7:
+                        self.swaps_left = min(self.swaps_left + 1, 2)
+                    elif tile == 8:
+                        self.deletes_left = min(self.deletes_left + 1, 2)
                     next_tile_used = True
                     empty_available = True
                     definitely_changed = True
@@ -132,9 +156,15 @@ class Game2048:
             self.tiles = new_tiles
             self.score = new_score
             self.moves += 1
-            return self._spawn()
+            spawn_level: typing.Literal[-1, 1, 2] = self._spawn()
+            self.moves_list.append(self.last_move)
+            self.last_move = (self.grid.copy(), self.score, self.game_state)
+            if len(self.moves_list) > self.moves_limit + 1:
+                self.moves_list.pop(0)
+            return spawn_level
 
-    def _check(self, check_argument: MoveOutput)-> GameState:
+    def _check(self, check_argument: typing.Literal[-1, 0, 1, 2])-> typing.Literal[-3, -2, -1, 1, 2, 3]:
+        """Check the game_state and update accordingly in case"""
         if check_argument != -1:
             max_number: int = 0
             for row in self.grid:
@@ -154,26 +184,62 @@ class Game2048:
         return self.game_state
 
     def public_move(self, direction: int)-> bool:
+        """Make a move in the game of 2048 in any direction
+        
+        Parameters:
+
+            direction = an integer, 0 means left, going up by 1 rotates the direction by 90 degrees clockwise
+        """
         if self.game_state == -1:
             return False
         self._check(self._move(direction))
         return True
 
-    def restart(self, custom_grid: Grid|None = None):
-        if custom_grid is None:
-            self.grid: Grid = [[0]*4]*4
-            self.game_state: GameState = 1
+    def undo(self)-> typing.Literal[-2, -1, 0]:
+        "Undo a move in the game of 2048"
+        if self.undos_left == 0:
+            return -1
+        elif len(self.moves_list) <= 1:
+            return -2
+        
+        new_game_position: typing.Tuple[typing.List[typing.List[int]], int, typing.Literal[-2, -1, 1, 2]] = self.moves_list.pop()
+        self.grid = new_game_position[0]
+        self.score = new_game_position[1]
+        self.game_state = new_game_position[2]
+        self.moves += 1
+        self.undos_left = max(self.undos_left - 1, -1)
+        self.powerups_used += 1
+        self.last_move = new_game_position
+        return 0
+
+    def restart(self, *, custom_grid: typing.List[typing.List[int]] | None = None, powerup_mode: typing.Literal[0, 1, 2] | None = None):
+        """Sets all the main variables, is used by __init__
+        
+        Parameters:
+
+            custom_grid = a 2D 4x4 list of integers, corresponding to the board the game will start with
+
+            powerup_mode = 0 (default) disables any powerups, 1 enables them and 2 starts the game with practice mode"""
+        self.grid: typing.List[typing.List[int]]
+        self.custom_grid: bool
+        self.original_grid: typing.List[typing.List[int]]
+        if hasattr(self, "original_grid") and custom_grid is None:
+            self.grid = self.original_grid
+        elif custom_grid is None:
+            self.grid = [[0]*4] * 4
+            self.original_grid = [[0]*4] * 4
+            self.custom_grid = False
         else:
-            self.grid: Grid = custom_grid
-            self.game_state: GameState = 3
+            self.grid: typing.List[typing.List[int]] = custom_grid.copy()
+            self.original_grid = custom_grid.copy()
+            self.custom_grid = True
+
+        self.game_state: typing.Literal[-2, -1, 1, 2] = 1
         self.tiles: typing.List[str] = ["", "2", "4"]
         self.score: int = 0
         self.moves: int = 0
         self._spawn()
         self._spawn()
-
-    def __init__(self, custom_grid: Grid|None = None):
-        self.restart(custom_grid)
         max_number: int = 0
         for row in self.grid:
             for tile in row:
@@ -182,35 +248,100 @@ class Game2048:
         while max_number >= len(self.tiles):
             self.tiles.append(str(2*int(self.tiles[-1])))
 
+        if hasattr(self, "powerup_mode") and powerup_mode is None:
+            powerup_mode = self.powerup_mode
+        elif powerup_mode is None:
+            self.powerup_mode = 0
+            powerup_mode = 0
+        else:
+            self.powerup_mode = powerup_mode
+        self.practice = bool(powerup_mode // 2)
+        self.powerups = bool(powerup_mode/2 + 0.5)
+        self.powerups_used: int = 0
+        if self.powerups:
+            self.undos_left: int = 2
+            self.swaps_left: int = 1
+            self.deletes_left: int = 0
+            self.moves_limit: int = 1
+        else:
+            self.undos_left: int = 0
+            self.swaps_left: int = 0
+            self.deletes_left: int = 0
+            self.moves_limit: int = 0
+        if self.practice:
+            self.undos_left: int = -1
+            self.moves_limit: int = 128
+        self.moves_list: typing.List[typing.Tuple[typing.List[typing.List[int]], int, typing.Literal[-2, -1, 1, 2]]] = [(self.grid.copy(), self.score, self.game_state)]
+        self.last_move: typing.Tuple[typing.List[typing.List[int]], int, typing.Literal[-2, -1, 1, 2]] = (self.grid.copy(), self.score, self.game_state)
+
+    def __init__(self, *, custom_grid: typing.List[typing.List[int]] | None = None, powerup_mode: typing.Literal[0, 1, 2] | None = None):
+        """Create a new game of 2048 object, its parameters are same self.restart() method"""
+        self.restart(custom_grid = custom_grid, powerup_mode = powerup_mode)
+
+
+def undo(game_object: Game2048):
+    """Undo a move in a game of 2048 and refresh the screen"""
+    result: typing.Literal[-2, -1, 0] = game_object.undo()
+    refresh(game_object)
+    if result == 0:
+        print("Last move undone")
+    elif result == -1:
+        print("You don't have any uses left, make 128 tiles to get more uses")
+    elif result == -2:
+        print("There is no move you can undo")
+
+
+def refresh(game_object: Game2048):
+    """Clear the command line and reprint a board of 2048"""
+    (lambda : os.system("cls") if os.name == "nt" else os.system("clear"))()
+    print(str(game_object))
+
 
 def restart(game_object: Game2048):
+    """Restart a game of 2048 and refresh the screen"""
     game_object.restart()
-    clean()
-    print(str(game_object))
+    refresh(game_object)
 
 
 def move(game_object: Game2048, direction: int):
+    """Do a move in a game of 2048 and refresh the screen
+    
+    Parameters:
+
+        direction = an integer, 0 means left, going up by 1 rotates the direction by 90 degrees clockwise
+    """
     game_object.public_move(direction)
-    clean()
-    print(str(game_object))
+    refresh(game_object)
 
 
 def main():
-    if input("By pressing enter, you agree that the terminal will be cleared and that the game of 2048 will start. Controls are WASD or arrow keys, ENTER to quit and R to restart. Press any key and enter to avoid starting the game: ") == "":
-        game = Game2048()
+    """Start the game of 2048 with keybinds on"""
+    start_input: str = input("By pressing enter, you agree that the terminal will be cleared and that the game of 2048 will start. Controls are WASD or arrow keys, ENTER to quit and R to restart. Write p to enable power ups (controlled by pressing SHIFT + U, S and T respectively) and add + at the end of the string to start in practice mode (unlimited undo's). Practice mode is impossible to enable without powerups. Write any invalid string and enter to avoid starting the game: ")
+    if start_input == "":
+        mode: int = 0
+    elif start_input == "p":
+        mode: int = 1
+    elif start_input == "p+":
+        mode: int = 2
+    else:
+        mode: int = -1
+    if mode >= 0:
+        game = Game2048(powerup_mode = mode)
         restart(game)
-        listener = keyboard.GlobalHotKeys({
-            "w": (lambda: move(game, 1)),
-            "s": (lambda: move(game, 3)),
-            "a": (lambda: move(game, 0)),
-            "d": (lambda: move(game, 2)),
-            "<up>": (lambda: move(game, 1)),
-            "<down>": (lambda: move(game, 3)),
-            "<left>": (lambda: move(game, 0)),
-            "<right>": (lambda: move(game, 2)),
-            "r": (lambda: restart(game))
-            
-        })
+        keybinds: typing.Dict[str, typing.Callable] = {
+            "w": lambda: move(game, 1),
+            "s": lambda: move(game, 3),
+            "a": lambda: move(game, 0),
+            "d": lambda: move(game, 2),
+            "<up>": lambda: move(game, 1),
+            "<down>": lambda: move(game, 3),
+            "<left>": lambda: move(game, 0),
+            "<right>": lambda: move(game, 2),
+            "r": lambda: restart(game)
+        }
+        if game.powerups:
+            keybinds["<shift>+u"] = lambda: undo(game)
+        listener = keyboard.GlobalHotKeys(keybinds)
         listener.start()
         print("Pressing enter will end the game, press any key to remove this text", end = "")
         input("")
